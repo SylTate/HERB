6c9cab8ddee6df597cf540ae2b44353fec997ec3
Clinton <nyabutoc@seas.upenn.edu>
2017-02-10 07:55:40 -0500

    add appliaction

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 from app import app
 application = app
-if __name__ == "main":
-        application.run(host='0.0.0.0',port=8080,debug=True)
+if __name__ == "__main__":
+        application.run(host='0.0.0.0',debug=True)
 #pp.run(debug=True,host='0.0.0.0')
