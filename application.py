d891d0a0a756b51b9843c31f3e97ea3357e8c5b6
Clinton <nyabutoc@seas.upenn.edu>
2017-02-10 07:50:24 -0500

    add appliaction object

--- application.py
+++ application.py
@@ -1,2 +1,5 @@
 from app import app
-app.run(debug=True,host = '0.0.0.0')
+application = app
+if __name__ == "main":
+        application.run(host='0.0.0.0',port=8080,debug=True)
+#pp.run(debug=True,host='0.0.0.0')
